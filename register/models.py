40234f754c0daefb2cdf29376ef8e26ac08baf73
Jhaykeeh <mayuelajuddkristoffer@gmail.com>
2025-11-30 23:08:11 +0800

    update(models.py): added a login ORM

--- register/models.py
+++ register/models.py
@@ -24,3 +24,10 @@ class Inventory(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.category})"
+#ORM FOR LOGIN
+class Login(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
